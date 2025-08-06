cd3b6789219181e7891edf3c74f209e14d2cdf31
Aabhas Nama <aabhasa9@gmail.com>
2025-08-06 18:06:52 +0530

    done

---
+++ src/routes/post.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware");
+const multer = require("multer");
+const { createPostController } = require("../controllers/post.controller");
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+const router = express.Router();
+
+router.post("/",
+     authMiddleware,
+     upload.single("image"),
+     createPostController
+);
+
+module.exports = router;
